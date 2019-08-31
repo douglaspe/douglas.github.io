3fe1c7b70ef3e12913d5ac3b093f739dbca133b0
Douglas <douglas@pe15@gmail.com>
2019-08-31 18:41:09 -0300

    first build

---
+++ precache-manifest.236793082a52b91ffe6818927b403cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d06d83d1c6f2b05433185478e2d3e57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a322165e93d40240fa",
+    "url": "/static/css/2.dd51b82d.chunk.css"
+  },
+  {
+    "revision": "b78bb6605fbf79b7373e",
+    "url": "/static/css/main.c20d863f.chunk.css"
+  },
+  {
+    "revision": "a4a322165e93d40240fa",
+    "url": "/static/js/2.10829d9a.chunk.js"
+  },
+  {
+    "revision": "b78bb6605fbf79b7373e",
+    "url": "/static/js/main.d25a6689.chunk.js"
+  },
+  {
+    "revision": "8eb43bfd130706b4cf71",
+    "url": "/static/js/runtime~main.1c95cdcd.js"
+  }
+]);
